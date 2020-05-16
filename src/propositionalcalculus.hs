c9fe0901464b79a2e80a9371bd342271d1f6a09f
Jeremy Mikkola <jeremy@jeremymikkola.com>
2020-05-16 15:05:00 -0400

    Add basic structure of a statement

--- src/propositionalcalculus.hs
+++ src/propositionalcalculus.hs
@@ -1,1 +1,17 @@
+data Statement
+  = Atom String
+  | Not Statement
+  | And Statement Statement
+  | Or Statement Statement
+  | Implies Statement Statement
+  deriving (Eq, Show)
+
+prettyPrint :: Statement -> String
+prettyPrint stmt = case stmt of
+  Atom    a   -> a
+  Not     a   -> "~" ++ prettyPrint a
+  And     a b -> "<" ++ prettyPrint a ++ "∧" ++ prettyPrint b ++ ">"
+  Or      a b -> "<" ++ prettyPrint a ++ "∨" ++ prettyPrint b ++ ">"
+  Implies a b -> "<" ++ prettyPrint a ++ "→" ++ prettyPrint b ++ ">"
+
 main = putStrLn "Hello world from propositionalcalculus"
