2621a3a35379a69596d4587d78f64d0cb161c6b2
Jeremy Mikkola <jeremy@jeremymikkola.com>
2020-05-16 17:13:46 -0400

    It seems to work

--- src/propositionalcalculus.hs
+++ src/propositionalcalculus.hs
@@ -1,4 +1,6 @@
+import Control.Monad (foldM)
 import Data.Char (isUpper)
+
 
 data Statement
   = Atom Char
@@ -24,6 +26,125 @@ data Rule
   | Demorgan Statement
   | Switch Statement
   deriving (Show)
+
+
+checkDerrivation :: Derrivation -> Result ()
+checkDerrivation (Derrivation steps) = do
+  _ <- foldM checkStep [] steps
+  return ()
+
+type Proven = [Statement]
+
+checkStep :: Proven -> Step -> Result Proven
+checkStep proven (newStatement, rule) = case rule of
+  Given -> do
+    requireProven proven newStatement
+    return proven
+
+  Join -> case newStatement of
+    (And a b) -> do
+      requireProven proven a
+      requireProven proven b
+      return (newStatement : proven)
+    _ ->
+      Left $ "join requires statement to be a (x & y), got " ++ prettyPrint newStatement
+
+  Separate stmt -> do
+    requireProven proven stmt
+    case stmt of
+      (And a _) | a == newStatement -> return (a : proven)
+      (And _ b) | b == newStatement -> return (b : proven)
+      (And _ _) -> Left $ prettyPrint newStatement ++ " is not part of " ++ prettyPrint stmt
+      _         -> Left $ "cannot separate " ++ prettyPrint stmt
+
+  DoubleNegation stmt -> do
+    requireProven proven stmt
+    if impliedByDoubleNegation newStatement stmt
+      then return (newStatement : proven)
+      else Left $ "cannot derrive " ++ prettyPrint newStatement ++ " from " ++
+           prettyPrint stmt ++ " by double negation"
+
+  Assumption stmt (Derrivation steps) -> do
+    innerProven <- foldM checkStep (stmt : proven) steps
+    let newTheorem = Implies stmt (head innerProven)
+    if newTheorem == newStatement
+      then return (newStatement : proven)
+      else Left $ "Assumption proved " ++ prettyPrint newTheorem ++
+           ", but goal was " ++ prettyPrint newStatement
+
+  Detachment stmt -> case stmt of
+    (Implies a b) -> do
+      requireProven proven stmt
+      requireProven proven a
+      if b == newStatement
+        then return (b : proven)
+        else Left $ "implication proves " ++ prettyPrint b ++
+             ", but goal is " ++ prettyPrint newStatement
+    _ ->
+      Left $ "detachment requires an implication, got " ++ prettyPrint stmt
+
+  Contrapositive stmt -> do
+    requireProven proven stmt
+    if isContraOf newStatement stmt || isContraOf stmt newStatement
+      then return (newStatement : proven)
+      else Left $ prettyPrint newStatement ++ " is not a contrapositive of " ++ prettyPrint stmt
+
+  Demorgan stmt -> do
+    requireProven proven stmt
+    edited <- applyDemorgan stmt
+    if newStatement == edited
+      then return (newStatement : proven)
+      else Left $ "cannot derrive " ++ prettyPrint newStatement ++ " from " ++
+           prettyPrint stmt ++ " with De Morgan's law"
+
+  Switch stmt -> do
+    requireProven proven stmt
+    if isSwitchOf stmt newStatement || isSwitchOf newStatement stmt
+      then return (newStatement : proven)
+      else Left  $ "cannot derrive " ++ prettyPrint newStatement ++ " from " ++
+           prettyPrint stmt ++ " with a switch"
+
+impliedByDoubleNegation :: Statement -> Statement -> Bool
+impliedByDoubleNegation s1 s2
+  | s1 == s2 = True
+  | otherwise = case (s1, s2) of
+      (a, Not (Not b)) -> impliedByDoubleNegation a b
+      (Not (Not a), b) -> impliedByDoubleNegation a b
+      (Not a, Not b) -> impliedByDoubleNegation a b
+      (And a1 a2, And b1 b2) ->
+        impliedByDoubleNegation a1 b1 && impliedByDoubleNegation a2 b2
+      (Or a1 a2, Or b1 b2) ->
+        impliedByDoubleNegation a1 b1 && impliedByDoubleNegation a2 b2
+      (Implies a1 a2, Implies b1 b2) ->
+        impliedByDoubleNegation a1 b1 && impliedByDoubleNegation a2 b2
+      _ ->
+        False
+
+isContraOf :: Statement -> Statement -> Bool
+isContraOf (Implies x y) (Implies notY notX) =
+  (Not x) == notX && (Not y) == notY
+isContraOf _ _ = False
+
+applyDemorgan :: Statement -> Result Statement
+applyDemorgan (And (Not x) (Not y)) =
+  return (Not (Or x y))
+applyDemorgan (Not (Or x y)) =
+  return (And (Not x) (Not y))
+applyDemorgan stmt =
+  Left $ "cannot apply De Morgan's law to " ++ prettyPrint stmt
+
+isSwitchOf :: Statement -> Statement -> Bool
+isSwitchOf (Or x1 y1) (Implies (Not x2) y2) =
+  x1 == x2 && y1 == y2
+isSwitchOf _ _ =
+  False
+
+requireProven :: Proven -> Statement -> Result ()
+requireProven proven stmt =
+  if stmt `elem` proven
+  then return ()
+  else Left $ "Cannot prove statement " ++ prettyPrint stmt ++ " given:\n" ++
+       unlines (map prettyPrint proven)
 
 prettyPrint :: Statement -> String
 prettyPrint stmt = case stmt of
@@ -202,14 +323,18 @@ requireNext c (x:xs) =
   then return xs
   else Left $ "required " ++ [c] ++ " but found " ++ (x:xs)
 
+checkFile :: String -> Either String ()
+checkFile content = do
+  parsed <- parseDerrivation content
+  checkDerrivation parsed
+
 run :: String -> IO ()
 run name = do
   content <- readFile name
-  case parseDerrivation content of
+  case checkFile content of
     Left err ->
       putStrLn err
-    Right result ->
-      putStrLn $ printIndent 0 result
+    Right _ ->
+      putStrLn "valid"
 
 main = run "examples/gantos_axe.txt"
-
