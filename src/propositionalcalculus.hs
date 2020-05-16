9bf3a1c296f126a1f065c1ee11075c4f739be555
Jeremy Mikkola <jeremy@jeremymikkola.com>
2020-05-16 16:33:36 -0400

    Add parsing and pretty printing

--- src/propositionalcalculus.hs
+++ src/propositionalcalculus.hs
@@ -8,8 +8,9 @@ data Statement
   | Implies Statement Statement
   deriving (Eq, Show)
 
-data Derrivation
-  = Derrivation [(Statement, Rule)]
+type Step = (Statement, Rule)
+
+data Derrivation = Derrivation [Step]
   deriving (Show)
 
 data Rule
@@ -32,17 +33,140 @@ prettyPrint stmt = case stmt of
   Or      a b -> "(" ++ prettyPrint a ++ " | " ++ prettyPrint b ++ ")"
   Implies a b -> "(" ++ prettyPrint a ++ " > " ++ prettyPrint b ++ ")"
 
+printIndent :: Int -> Derrivation -> String
+printIndent indent (Derrivation steps) =
+  unlines $ map (printStep indent) steps
+
+printStep :: Int -> Step -> String
+printStep indent (stmt, rule) =
+  showIndent indent ++ prettyPrint stmt ++ printRule indent rule
+
+showIndent :: Int -> String
+showIndent n =
+  if n < 1 then "" else "  " ++ showIndent (n - 1)
+
+printRule :: Int -> Rule -> String
+printRule indent rule = case rule of
+  Given                  -> ""
+  Join                   -> " <- join"
+  Separate       stmt    -> " <- separate "       ++ prettyPrint stmt
+  DoubleNegation stmt    -> " <- doublenegation " ++ prettyPrint stmt
+  Detachment     stmt    -> " <- detach "         ++ prettyPrint stmt
+  Contrapositive stmt    -> " <- contrapositive " ++ prettyPrint stmt
+  Demorgan       stmt    -> " <- demorgan "       ++ prettyPrint stmt
+  Switch         stmt    -> " <- switch "         ++ prettyPrint stmt
+  Assumption stmt derriv ->
+    " <- [\n" ++
+    showIndent (indent + 1) ++ prettyPrint stmt ++ "\n" ++
+    printIndent (indent + 1) derriv ++
+    showIndent indent ++ "]"
+
 type Result a = Either String a
 
 parseStatement :: String -> Result Statement
 parseStatement s = do
   (stmt, rest) <- parseNext s
-  if rest /= ""
+  if nonBlank rest
     then Left $ "left over after statement: " ++ rest
     else return stmt
 
 parseDerrivation :: String -> Result Derrivation
-parseDerrivation = undefined
+parseDerrivation s = do
+  steps <- parseSteps (filter nonBlank $ lines s) []
+  return $ Derrivation steps
+
+nonBlank :: String -> Bool
+nonBlank "" = False
+nonBlank (c:cs) = case c of
+  ' ' -> nonBlank cs
+  '#' -> False -- this starts a comment
+  _   -> True -- any other character is probably part of a statement
+
+isBlank = not . nonBlank
+
+parseSteps :: [String] -> [Step] ->  Result [Step]
+parseSteps [] steps =
+  return $ reverse steps
+parseSteps (line:ls) steps = do
+  (stmt, lineRest) <- parseNext line
+  (rule, lines) <- parseRule lineRest ls
+  let step = (stmt, rule)
+  parseSteps lines (step : steps)
+
+parseRule :: String -> [String] -> Result (Rule, [String])
+parseRule lineRest lines
+  | isBlank lineRest = return (Given, lines)
+  | otherwise = do
+      remainder <- readArrow lineRest
+      let (ruleName, rest) = splitFirst remainder
+      case ruleName of
+        "join" -> do
+          requireBlank rest
+          return (Join, lines)
+        "separate" ->
+          parseRuleFor Separate rest lines
+        "doublenegation" ->
+          parseRuleFor DoubleNegation rest lines
+        "detach" ->
+          parseRuleFor Detachment rest lines
+        "contrapositive" ->
+          parseRuleFor Contrapositive rest lines
+        "demorgan" ->
+          parseRuleFor Demorgan rest lines
+        "switch" ->
+          parseRuleFor Switch rest lines
+        "[" -> do
+          requireBlank rest
+          parseAssumption lines
+        _ ->
+          Left $ "invalid rule name: " ++ ruleName
+
+
+parseAssumption :: [String] -> Result (Rule, [String])
+parseAssumption [] = Left "unexpected end of assumption"
+parseAssumption (l:ls) = do
+  assumedStmt <- parseStatement l
+  (steps, remainingLines) <- parseAssumptionSteps ls []
+  return (Assumption assumedStmt (Derrivation steps), remainingLines)
+
+parseAssumptionSteps :: [String] -> [Step] -> Result ([Step], [String])
+parseAssumptionSteps [] steps = Left "unfinished assumption"
+parseAssumptionSteps (l:ls) steps
+  | trim l == "]" =
+    return (reverse steps, ls)
+  | otherwise = do
+      (stmt, lineRest) <- parseNext l
+      (rule, lines) <- parseRule lineRest ls
+      let step = (stmt, rule)
+      parseAssumptionSteps lines (step : steps)
+
+parseRuleFor :: (Statement -> Rule) -> String -> [String] -> Result (Rule, [String])
+parseRuleFor builder rest lines = do
+  stmt <- parseStatement rest
+  return (builder stmt, lines)
+
+readArrow :: String -> Result String
+readArrow ('<':'-':rest) = return (eatWhitespace rest)
+readArrow (' ':rest) = readArrow rest
+readArrow s = Left $ "Expected <-, got " ++ s
+
+eatWhitespace :: String -> String
+eatWhitespace (' ':rest) = eatWhitespace rest
+eatWhitespace s = s
+
+trim = eatWhitespace . reverse . eatWhitespace . reverse
+
+splitFirst :: String -> (String, String)
+splitFirst "" = ("", "")
+splitFirst (' ':s) = ("", eatWhitespace s)
+splitFirst (c:s) =
+  let (before, after) = splitFirst s
+  in (c:before, after)
+
+requireBlank :: String -> Result ()
+requireBlank s =
+  if isBlank s then return ()
+  else Left $ "Expected the end of a line, got " ++ s
 
 parseNext :: String -> Result (Statement, String)
 parseNext "" = Left "empty statement"
@@ -78,4 +202,14 @@ requireNext c (x:xs) =
   then return xs
   else Left $ "required " ++ [c] ++ " but found " ++ (x:xs)
 
-main = putStrLn "Hello world from propositionalcalculus"
+run :: String -> IO ()
+run name = do
+  content <- readFile name
+  case parseDerrivation content of
+    Left err ->
+      putStrLn err
+    Right result ->
+      putStrLn $ printIndent 0 result
+
+main = run "examples/gantos_axe.txt"
+
